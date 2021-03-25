5b07a849308b0360639da9301629ccd086683de4
Hyzam Ali <hyzam@Hyzams-MacBook-Pro.local>
2021-03-25 16:23:40 +0530

    minor fixes

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
+import os
+
 from flask import Flask
 from flask_restful import Api
 from flask_jwt import JWT
+
 from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
@@ -8,6 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.secret_key = "blahblah"
+app.config["DEBUG"] = True
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
@@ -22,4 +26,10 @@ api.add_resource(UserRegister,"/register")
 if __name__ == "__main__":
 	from db import db
 	db.init_app(app)
+
+	if app.config["DEBUG"]:
+		@app.before_first_request
+        def create_tables():
+            db.create_all()
+            
 	app.run(port = "5000", debug = True)
