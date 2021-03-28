63e67f7ea984a848134dc725584d786f20999d45
Hyzam Ali <hyzam@Hyzams-MacBook-Pro.local>
2021-03-28 12:31:14 +0530

    fixes

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.secret_key = "blahblah"
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
-print(os.environ.get("DATABASE_URL", "sqlite:///data.db"))
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db" #os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
