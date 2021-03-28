4d571b5f2efb8872bd355c64aca7958f595e0e6e
Hyzam Ali <hyzam@Hyzams-MacBook-Pro.local>
2021-03-28 11:46:55 +0530

    added provision for Postgres

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 app.secret_key = "blahblah"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 api = Api(app)
 jwt = JWT(app, authenticate, identity)
